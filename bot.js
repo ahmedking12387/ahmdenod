fb94da2aa70fb0c6d4a48110784fc3d1d9fded51
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-17 14:46:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = '+'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -27,5 +27,81 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+
+
+client.on("message", message => {
+    if (message.content === (prefix + "help")) {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#580e6b")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`
+
+
+('+id ' , 'لعرض معلوماتك') 
+('+server' , 'لمعلومات سيرفر') 
+('رابط السيرفر الي تكتب الاوامر بيه' ,' رابط  ')
+('+invite   ' ,' لاضافه البوت   ')
+('+roles' ,'لاضهار رتب  سيرفر بالترتيب')
+('+infobot ' ,'معلومات البوت')
+('+bc ' , 'لارسال رساله جماعية')
+('+v2min ' , 'لانشاء روم مؤقت بتأكيد')
+('+ping ' , 'سرعه بوت')
+('+vc ' , 'كيك فويس')
+('removeRoles ' , 'لحذف جميع رتب')
+('+invites ' , 'لمعرفه عدد الاعضاء الذي دخلوا عبرك')
+('+rmutechannel ' , 'لقفل الشات')
+('+runmutechannel ' , 'لفتح الشات')
+('+rooms ' , 'لاضهار الرومات')
+('+ct ' , 'لانشاء روم كتابي')
+('+cv ' , 'لانشاء روم صوتي')
+('+delet ' , 'لخذف روم صوتي او شات')
+('+calculator ' , 'الالة الحسابية')
+('+unhide ' , 'لفتح جميع الرومات صوتية وكتابية')
+('+content ' , 'لارسال اقتراح لصحاب البوت')
+('+move ' , 'لسحب الشخص في رومات صوتية')
+('+uptime ' , 'لمعرفه مده تشغيل البوت')
+('+member ' , 'حالة الاعضاء')
+('+serverimage ' , 'لاضهار صوره السيرفر')
+('+image ' , 'لاضهار صورتك')
+('roleadd ' , 'لاعطاء رتبه')
+('+roleremove ' , 'لازاله الرتبه')
+('+nickname ' , 'لتغير اسم العضو')
+('+suppport ' , 'لحصول على سيرفر المساعدة')
+('+Mute ' , 'لاعطاء ميوت شات مع سبب')
+('UnMute ' , 'لفك ميوت شات')
+('+clear ' , 'لمسح الشات حد اقصى 200رساله') 
+('+serooms' , 'انشاء رومات جاهزه')
+('+seroles' , 'انشاء رتب جاهزه')
+('+removerooms' , 'لازالة جميع الرومات')
+('+createcolors' , 'لانشاء 137 الوان')
+('+deletecolors' , 'لحذف جميع الوان')
+('+hide' , 'لاخفاء جميع رومات في سسيرفر')
+('+tag' , 'لزخرفه الكتابه للانكليزي فقط')
+('+sd' , 'لحذف كل شيئ بسيرفر')
+('+removeroles' , 'لحذف جميع رتب')
+('+unhide' , 'لفتح جميع رومات')
+('+ban' , 'لاعطاء باند')
+('+unbans' , 'لفك باند عن جميع')
+('لاتخيار الوان الذي تريده' , 'لون')
+('+owner' , 'كود تأكد من ملكيه البوت')
+تحذير :البوت يمتلك لوق يعمل في روم يسمى log
+البوت يقوم بطرد من ينشر سيرفر تحذير
+
+`)
+   message.author.sendEmbed(embed)
+   
+   }
+   });  
+client.on('message', message => {
+     if (message.content === (prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#8650a7")
+  .addField("Done" , " تــــم ارســالك في الخــاص")
+  message.channel.sendEmbed(embed);
+    }
+});
+
 
 client.login(process.env.BOT_TOKEN);
