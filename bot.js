46b850e2469c2bc4f10bc091164524b3ad946609
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-17 17:40:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,6 +104,7 @@ client.on('message', message => {
 });
 
 
+
 client.on('message', message => {
 	const prefix = '+'
 var args = message.content.split(" ").slice(1);    
@@ -158,7 +159,6 @@ message.channel.send({embed});
 });
 
 
-
 client.on('message', function(msg) {
 	const prefix = '+'
     if(msg.content.startsWith (prefix  + 'server')) {
@@ -178,6 +178,38 @@ client.on('message', function(msg) {
       msg.channel.send({embed:embed});
     }
   });
+
+
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+---------------------
+-[${message.guild.name}]  هذا هو رابط سيرفر
+---------------------
+-هذا الرابط صالح ل 100 مستخدم فقط
+---------------------
+-هذا الرابط صالح لمده 24 ساعه فقط
+---------------------
+**`)
+      message.author.sendEmbed(Embed11)
+    }
+});
 
 
 client.login(process.env.BOT_TOKEN);
