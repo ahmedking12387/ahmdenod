82a47d42046ea2ea38b936fdaecfdf14538f680c
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-18 11:29:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -687,14 +687,42 @@ client.on('message' , message => {
 
 
 client.on('message', message => {
-if (message.content.startsWith("+kick")) {
-    var mention = message.mentions.members.first();
-    if(!mention) return message.channel.send("يجب منشن العضو");
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    mention.kick("By: " + message.author.tag);
-    
-    message.channel.send("تم أعطاء كيك الى : " + mention.tag);
-};
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
+
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .kickable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+  message.guild.member(user).kick();
+
+  const kickembed = new Discord.RichEmbed()
+  .setAuthor(`KICKED!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : kickembed
+  })
+}
 });
 
 
@@ -943,48 +971,6 @@ client.on("guildMemberAdd", member => {
 
 
 
-const cuttweet = [
-     'كت تويت ‏- تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
-     'كت تويت ‏- أكثر شيء يُسكِت الطفل برأيك؟',
-     'كت تويت ‏- الحرية لـ ... ؟',
-     'كت تويت ‏- قناة الكرتون المفضلة في طفولتك؟',
-     'كت تويت ‏- كلمة للصُداع؟',
-     'كت تويت ‏- ما الشيء الذي يُفارقك؟',
-     'كت تويت ‏- ما الشيء الذي يُفارقك؟',
-     'كت تويت ‏- موقف مميز فعلته مع شخص ولا يزال يذكره لك؟',
-     'كت تويت ‏- أيهما ينتصر، الكبرياء أم الحب؟',
-     'كت تويت| بعد ١٠ سنين ايش بتكون ؟',
-
-     'كت تويت ‏- مِن أغرب وأجمل الأسماء التي مرت عليك؟',
-     '‏كت تويت| عمرك شلت مصيبة عن شخص برغبتك ؟',
-'كت تويت ‏- أكثر سؤال وجِّه إليك مؤخرًا؟',
-     '‏كت تويت|ما هو الشيء الذي يجعلك تشعر بالخوف؟',
-     '‏كت تويت|وش يفسد الصداقة؟',
-     '‏كت تويت|شخص لاترفض له طلبا ؟',
-     '‏كت تويت|كم مره خسرت شخص تحبه؟.',
-     '‏كت تويت|كيف تتعامل مع الاشخاص السلبيين ؟',
-     '‏كت تويت|كلمة تشعر بالخجل اذا قيلت لك؟',
-     '‏كت تويت|هل تُخفي نجاحك أو كت كت تويت | هل تخفي نجاحك أو أشيائك الجميلة خوفاً من العين والحسد؟',
-     '‏كت تويت|جسمك اكبر من عٌمرك او العكسّ ؟!',
-     '‏كت تويت|أقوى كذبة مشت عليك ؟',
-     '‏كت تويت|تتأثر بدموع شخص يبكي قدامك قبل تعرف السبب ؟',
-     'كت تويت|هل حدث وضحيت من أجل شخصٍ أحببت؟',
-     '‏كت تويت|أكثر تطبيق تستخدمه مؤخرًا؟',
-     '‏كت تويت|‏اكثر شي يرضيك اذا زعلت بدون تفكير ؟',
-     '‏كت تويت|وش محتاج عشان تكون مبسوط ؟',
-     '‏كت تويت|مطلبك الوحيد الحين ؟',
-     '‏كت تويت|- هل حدث وشعرت بأنك ارتكبت أحد الذنوب أثناء الصيام؟',
-]
-
-client.on('message', message => {
-  if (message.content === `-كت تويت`) {
-message.channel.sendMessage({embed: {
-  color: 3547003,
-  description: `${cuttweet[Math.floor(Math.random() * cuttweet.length)]}`
-}});
-};
-});
-
 
 client.on('message', message => {
   if (message.author.bot) return;
@@ -1013,6 +999,7 @@ if (command == "+embed") {
 
 
 });
+
 
 
 client.login(process.env.BOT_TOKEN);
