f480278ff1f85b8058eabf9c5415ed2a4bb9c23f
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-18 10:00:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -730,6 +730,20 @@ client.on('message', message => {
   
  
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  if(!message.channel.guild) return;
+  if(!message.member.hasPermission('MANAGE_MESSAGES')) return;
+  if (message.mentions.users.size < 1) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+ 
+
 if (command == "+warn") {
     let say = new Discord.RichEmbed()
     .setDescription(args.join("  "))
@@ -745,5 +759,6 @@ if (command == "+warn") {
 
 
 });
+	
 
 client.login(process.env.BOT_TOKEN);
