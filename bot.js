93040731025cd1ffdc7d693f3ad3915ecf200894
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-18 14:26:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '+'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`+help|Ip:FlixMC.tk  .`,"http://twitch.tv/S-F")
+client.user.setGame(`+help | Ip : FlixMC.tk  .`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
