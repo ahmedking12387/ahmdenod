e4e5a74978db0774165473f704c93d181877da47
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-22 04:47:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1175,17 +1175,40 @@ client.on("message", message => {
 
 
 
-bot.on('ready', () => {
-    console.log(`[Start] ${new Date()}`);
-    console.log(`[INFO] ${bot.user.username}`)
-    console.log(`[INFO] ${bot.users.size}`)
-    console.log(`[INFO] ${bot.guilds.size}`)
-    console.log(`[BOT] Auto Role `)
+client.on('guildMemberAdd', (member) => {
+member.addRole(member.guild.roles.find('name', 'not active'));
 });
 
-bot.on('guildMemberAdd', (member) => {
-member.addRole(member.guild.roles.find('name',FlixCommunity's Citizen));
-});
+
+client.on('message', message => {                      
+    if(!message.channel.guild) return;
+       if(message.content.startsWith(prefix + 'active')) {
+        let modlog = client.channels.find('name', 'chat');
+       if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+       message.channel.sendMessage(`اضغط على الصح عشان تتفعل`).then(msg => {
+        
+        
+        msg.react('✅')
+       .then(() => msg.react('✅'))
+     
+     
+
+       let activeFilter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+     
+       let active = msg.createReactionCollector(activeFilter, { time: 15000 });
+     
+                                                        
+                               active.on("collect", r => {
+                                   message.member.addRole(message.guild.roles.find("name", "active"));
+                                   message.member.removeRole(message.guild.roles.find("name", "not active"));
+                                   msg.delete();
+                                   message.channel.send(`**تم تفعيلك استمتع.**`).then(m => m.delete(1000));
+     
+                                   })
+                                   })
+                                   }
+                                   });
+
 
 
 client.login(process.env.BOT_TOKEN);
