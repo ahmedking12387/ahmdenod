7d4174f4623432bb7e619a662daf05099d47366e
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-23 17:44:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '+'
 
-
-t.user.setGame(`+help |   IP:FlixMC.tk  .`,"http://twitch.tv/A_K")
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`+help |   IP:FlixMC.tk  .`,"http://twitch.tv/A_K")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
