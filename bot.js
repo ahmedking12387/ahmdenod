9ce7f652360c56678e8a27f3fa810ea2439012e8
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-23 23:12:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1615,11 +1615,146 @@ client.on('message' , message => {
 
 
 client.on('message', message => {
-   if (message.content === "*قرعه 25") {
+   if (message.content === "+قرعه 25") {
   message.channel.sendMessage(Math.floor(Math.random() * 25));
     }
 });
 
+
+
+  client.on("ready", () => {
+
+    var guild;
+
+    while (!guild)
+
+        guild = client.guilds.get("اي دي سيرفرك - Server id");
+
+    guild.fetchInvites().then((data) => {
+
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+
+            dat[Inv] = Invite.uses;
+
+        });
+
+    });
+
+});
+
+ 
+
+ 
+
+ 
+
+client.on("guildMemberAdd", (member) => {
+
+    let channel = member.guild.channels.get("451762950391201794");
+
+    if (!channel) {
+
+        console.log("!the channel id it's not correct");
+
+        return;
+
+    }
+
+    if (member.id == client.user.id) {
+
+        return;
+
+    }
+
+    console.log('-');
+
+    var guild;
+
+    while (!guild)
+
+        guild = client.guilds.get("342720209540612096");
+
+    guild.fetchInvites().then((data) => {
+
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+
+            if (dat[Inv])
+
+                if (dat[Inv] < Invite.uses) {
+
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;       
+
+ }
+
+            dat[Inv] = Invite.uses;
+
+       
+
+       });
+
+    });
+
+});
+
+
+
+ var request = require('request');
+
+var mcCommand = '+minecraft'; // Command for triggering
+
+var mcIP = 'FlixMC.tk'; // Your MC server IP
+
+var mcPort = 25565; // Your MC server port
+
+
+client.on('message', message => {
+
+    if (message.content === mcCommand) {
+
+        var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+
+        request(url, function(err, response, body) {
+
+            if(err) {
+
+                console.log(err);
+
+                return message.reply('Error getting Minecraft server status...');
+
+            }
+
+            body = JSON.parse(body);
+
+            var status = '*Minecraft server is currently offline*';
+
+            if(body.online) {
+
+                status = '**Minecraft** server is **online** - ';
+
+                if(body.players.now) {
+
+                    status += '**' + body.players.now + '** people are playing!';
+
+                } else {
+
+                    status += '*Nobody is playing!*';
+
+                }
+
+            }
+
+            message.reply(status);
+
+        });
+
+    }
+
+});
+ 
 
 
 client.login(process.env.BOT_TOKEN);
