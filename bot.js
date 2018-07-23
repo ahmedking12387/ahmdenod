46c852670f6e488aaa7a40273f02a1239ae665bb
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-24 00:34:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1839,8 +1839,6 @@ var mentionned = message.mentions.members.first();
 client.on('voiceStateUpdate', (Codes, ReBeL) => {
 client.channels.get(channel);
     ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "انا قوووي"))
-                  ReBeL.guild.createChannel('ReBeL', 'voice').then(c => {
-                ReBeL.setVoiceChannel(c).then(() => {
                     c.delete(305).catch(console.log);
   console.log("Done");
 });
