846ce75c574dcd3d0d0fb15c78f93c8089e00d66
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-25 20:53:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1117,5 +1117,58 @@ client.on('message', message => {
     }
 });
 
+ var request = require('request');
+
+var mcCommand = '+minecraft'; // Command for triggering
+
+var mcIP = 'FlixMC.tk'; // Your MC server IP
+
+var mcPort = 25565; // Your MC server port
+
+
+client.on('message', message => {
+
+    if (message.content === mcCommand) {
+
+        var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+
+        request(url, function(err, response, body) {
+
+            if(err) {
+
+                console.log(err);
+
+                return message.reply('Error getting Minecraft server status...');
+
+            }
+
+            body = JSON.parse(body);
+
+            var status = '*Minecraft server is currently offline*';
+
+            if(body.online) {
+
+                status = '**Minecraft** server is **online** - ';
+
+                if(body.players.now) {
+
+                    status += '**' + body.players.now + '** people are playing!';
+
+                } else {
+
+                    status += '*Nobody is playing!*';
+
+                }
+
+            }
+
+            message.reply(status);
+
+        });
+
+    }
+
+});
+
 client.login(process.env.BOT_TOKEN);
 
