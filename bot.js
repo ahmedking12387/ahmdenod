d61f963790ee64f75d7414160c5f69e7d3bdaec6
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-07-29 05:29:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -263,33 +263,12 @@ client.on('message', message => {
 });
 
 
-client.on('message', msg => {
-  if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefix)) return;
-  let command = msg.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = msg.content.split(" ").slice(1);
 
-    if(command === "purge") {
-        const emoji = client.emojis.find("name", "wastebasket")
-    let textxt = args.slice(0).join("");
-    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
-    if (textxt == "") {
-    msg.channel.send("ضع عددا من الرسائل التي تريد مسحها");
-} else {
-    msg.delete().then
-    msg.delete().then
-    msg.channel.bulkDelete(textxt);
-    msg.channel.send(`${emoji} Deleted ` + "`" + textxt + "` messages");
-        }    
-    }
-}
-});
 
 
 
 client.on('message', message => {
-   if (message.content === "*قرعه 25") {
+   if (message.content === "+قرعه 25") {
   message.channel.sendMessage(Math.floor(Math.random() * 25));
     }
 });
