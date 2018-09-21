33aa82bb7d49d5189c59f2bb2e2730f03d92c25e
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-09-22 01:50:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -614,7 +614,7 @@ client.on('message', function(message) {
     if (message.channel.type === "dm") {
         if (message.author.id === client.user.id) return;
         var stewart = new Discord.RichEmbed()
-            .setColor('RANDOM')c
+            .setColor('RANDOM')
             .setTimestamp()
             .setTitle('``رساله جديده في خاص البوت``')
             .setThumbnail(`${message.author.avatarURL}`)
