21a3597e6d8bf5064a7f2d6684b55f77afbbef62
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-10-22 21:51:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,19 @@ client.on('ready', () => {
   console.log('')
 });
 
+
+client.on('message', message => {
+    if(message.content.startsWith(prefix + "r")){
+        if(message.author.id !== "279194403564814336") return message.reply(`م تقدر تسوي ريستارت الا اذا كنت اونر البوت`);
+        client.destroy();
+        client.login('BOT_TOKEN');
+        message.channel.send(`<@${message.author.id}> , restart (: `)
+        
+    }
+});
+
+
+
 client.on("message", message => {
     var args = message.content.split(' ').slice(1);
     var msg = message.content.toLowerCase();
@@ -437,64 +450,7 @@ let types = [
 
 
 
-client.on("message",function(message) {
-    if(message.content.startsWith(prefix + "stats")) {
-           let uptime = client.uptime;
 
-    let days = 0;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let notCompleted = true;
-
-    while (notCompleted) {
-
-        if (uptime >= 8.64e+7) {
-
-            days++;
-            uptime -= 8.64e+7;
-
-        } else if (uptime >= 3.6e+6) {
-
-            hours++;
-            uptime -= 3.6e+6;
-
-        } else if (uptime >= 60000) {
-
-            minutes++;
-            uptime -= 60000;
-
-        } else if (uptime >= 1000) {
-            seconds++;
-            uptime -= 1000;
-
-        }
-
-        if (uptime < 1000)  notCompleted = false;
-
-    }
-let ms = 1000;
-let v1 = new Discord.RichEmbed()
-  v1.setTimestamp(new Date())
-  v1.setColor("RED")
-  v1.setDescription('***__ Collecting Data __***')
-  v1.setFooter("# | SenioR |")
-let heroo = new Discord.RichEmbed()
-.setColor('RANDOM')
-.setTimestamp(new Date())
-.setThumbnail(client.user.avatarURL)
-.setAuthor(client.user.username,client.user.avatarURL)
-.addField("MyPrefix :",`**[ ${prefix} ]**`,true)
-.addField("Guilds :","**[ "+client.guilds.size+" ]**",true)
-.addField("Channels :","**[ "+client.channels.size+" ]**",true)
-.addField("Users :","**[ "+client.users.size+" ]**",true)
-.addField("MyName : ","**[ "+client.user.username+" ]**",true)
-.addField("MyID :","**[ "+client.user.id+" ]**",true)
-.addField("UpTime :",`**[** **Days:** \`${days}\` **Hours:** \`${hours}\` **Minutes:** \`${minutes}\` **Seconds:** \`${seconds}\` **]**`,true)
-.setFooter("Wind |System")
-  message.channel.send({embed:v1}).then(m => m.edit({embed:heroo})),ms;
-    }
-});
 
 client.on('message', function(message) {
     if (message.channel.type === "dm") {
